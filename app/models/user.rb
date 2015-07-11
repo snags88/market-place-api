9e5320b5a0cd621a6ee5f02c6b0b80fc6e15333c
Seiji Naganuma <snaganumas@gmail.com>
2015-07-11 15:15:09 -0400

    implement auth_token on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :auth_token, uniqueness: true
+
+  before_create :generate_authentication_token!
+
+  def generate_authentication_token!
+    begin
+      token = Devise.friendly_token
+    end while User.where(auth_token: token).exists?
+    self.auth_token = token
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let!(:user){create(:user)}
   describe 'user validation' do
     it 'initializes with factory' do
       expect{create(:user)}.to_not raise_error
+    end
+    it 'has unique tokens' do
+      should validate_uniqueness_of(:auth_token)
+    end
+  end
+
+  describe '#generate_authentication_token!' do
+    it 'generates a unique token' do
+      allow(Devise).to receive(:friendly_token) {"auniquetoken123"}
+      # Devise.stub(:friendly_token).and_return()
+      user.generate_authentication_token!
+      expect(user.auth_token).to eq("auniquetoken123")
+    end
+    it 'generates another token when one is already taken' do
+      existing_user = create(:user, auth_token: "auniquetoken123")
+      user.generate_authentication_token!
+      expect(user.auth_token).to_not eq("auniquetoken123")
     end
   end
 end
