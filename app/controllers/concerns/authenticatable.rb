3a3877f6c0e0502695aee0853525a16563446c34
Seiji Naganuma <snaganumas@gmail.com>
2015-07-11 16:37:02 -0400

    implement authenticate wiht token

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -3,7 +3,12 @@ module Authenticatable
     @current_user ||= User.find_by(auth_token: auth_token(request))
   end
 
-  def auth_token(request=nil)
-    request.headers['Authorization'] if request
+  def authenticate_with_token
+    render json: {errors: "Not authenticated"}, status: 401 unless current_user.present?
   end
+
+  private
+    def auth_token(request=nil)
+      request.headers['Authorization'] if request
+    end
 end
--- spec/controllers/concerns/authenticatable_spec.rb
+++ spec/controllers/concerns/authenticatable_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
-class Authentication
-  attr_accessor :request
+class Authentication < ApiController
   include Authenticatable
 end
 
 describe Authenticatable do
   let(:authentication) {Authentication.new}
-
+  let(:user){create(:user)}
   describe '#current_user' do
-    let(:user){create(:user)}
     before do
       request.headers["Authorization"] = user.auth_token
       allow(authentication).to receive(:request) {request}
@@ -19,4 +17,24 @@ describe Authenticatable do
       expect(authentication.current_user).to eq(user)
     end
   end
+
+  describe '#authenticate_with_token' do
+    before(:each) do
+      allow(authentication).to receive(:current_user){nil}
+      allow(response).to receive(:response_code){401}
+      allow(response).to receive(:body) do
+        {"errors" => "Not authenticated"}.to_json
+      end
+      allow(authentication).to receive(:response){response}
+    end
+
+    xit 'renders a json error message' do
+      expect(json_response[:errors]).to eq("Not authenticated")
+    end
+
+    xit 'responds with 401' do
+      should respond_with 401
+    end
+  end
+
 end
