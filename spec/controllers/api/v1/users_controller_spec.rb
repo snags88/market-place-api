dafc11b099987dd3164b7c7f811eaba96d42d7db
Seiji Naganuma <snaganumas@gmail.com>
2015-07-11 12:29:35 -0400

    add post create specs

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -17,4 +17,39 @@ describe Api::V1::UsersController do
 
     it { should respond_with 200 }
   end
+
+  describe 'POST #create' do
+    context 'when resource is successfully created' do
+      let(:attributes){attributes_for(:user)}
+
+      before(:each) do
+        post :create, {user: attributes}, format: :json
+      end
+
+      it 'renders the json representation for the user record just created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(attributes[:email])
+      end
+      it {should respond_with 201}
+    end
+
+    context 'when resource is not created' do
+      let(:attributes){attributes_for(:user, email: nil)}
+
+      before(:each) do
+        post :create, {user: attributes}, format: :json
+      end
+
+      it 'renders an error json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+      it 'renders json with reason of failure' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include("can't be blank")
+      end
+      it {should respond_with 422}
+    end
+
+  end
 end
